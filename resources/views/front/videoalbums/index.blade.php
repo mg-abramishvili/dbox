703edb098cdce3bd968b73e6b74da405607220a8
mg-abramishvili <mg@abramishvili.net>
2021-02-16 03:18:12 +0500

    .

--- resources/views/front/videoalbums/index.blade.php
+++ resources/views/front/videoalbums/index.blade.php
@@ -4,7 +4,12 @@
 <div class="wrapper" style="">
 
         @if($settings->theme == 'default')
-            @include('front.videoalbums.index-default')
+            @if($settings->orientation == 'horizontal')
+                @include('front.videoalbums.default.index-horizontal')
+            @elseif($settings->orientation == 'vertical')
+                @include('front.videoalbums.default.index-vertical')
+            @endif
+
         @elseif($settings->theme == 'nast')
             @include('front.videoalbums.index-nast')
         @elseif($settings->theme == 'kadet')
