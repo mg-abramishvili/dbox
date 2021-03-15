25f3bcb2449871c29dc09677c039b75ce274c789
mg-abramishvili <mg@abramishvili.net>
2021-03-15 14:41:08 +0500

    .

--- resources/views/front/pages/show.blade.php
+++ resources/views/front/pages/show.blade.php
@@ -5,12 +5,19 @@
 
         @if($settings->theme == 'default')
             @include('front.pages.default.show')
+        
         @elseif($settings->theme == 'nast')
             @include('front.pages.nast.show')
+        
         @elseif($settings->theme == 'detsad')
             @include('front.pages.detsad.show')
+
+        @elseif($settings->theme == 'med')
+            @include('front.pages.med.show')
+
         @elseif($settings->theme == 'book')
             @include('front.pages.book.show')
+
         @endif
 
 </div>
